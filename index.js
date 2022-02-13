ce97c803cf8405789e31d55f2465ded8a898fe89
scrof90 <asknyshev@gmail.com>
2022-02-13 03:41:47 +0200

    remove hardcoded data

--- index.js
+++ index.js
@@ -31,31 +31,6 @@ app.use(logger);
 app.use(cors());
 app.use(express.static('build'));
 
-// data
-
-let people = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
 });

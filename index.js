cb55d0a891d596d6dd4447c7cbf42a45bffd7378
scrof90 <asknyshev@gmail.com>
2022-02-15 03:32:48 +0200

    add validation for name field

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get('/api/people/:id', (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post('/api/people', (request, response) => {
+app.post('/api/people', (request, response, next) => {
   const body = request.body;
 
   if (body.name === undefined) {
@@ -69,10 +69,13 @@ app.post('/api/people', (request, response) => {
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    console.log(savedPerson);
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      console.log(savedPerson);
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/people/:id', (request, response, next) => {
@@ -84,14 +87,13 @@ app.delete('/api/people/:id', (request, response, next) => {
 });
 
 app.put('/api/people/:id', (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' },
+  )
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -122,6 +124,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
